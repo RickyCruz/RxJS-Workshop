1528df502e0fceed2787eb25f210407730a6be01
Ricky Cruz <rczdev@gmail.com>
2020-06-29 19:51:14 -0500

    takeWhile

---
+++ src/operators/12-takeWhile.ts
@@ -0,0 +1,15 @@
+import { fromEvent } from 'rxjs';
+import { map, takeWhile } from 'rxjs/operators';
+
+const click$ = fromEvent<MouseEvent>(document, 'click');
+
+click$.pipe(
+    map( ({ x, y }) => ({ x, y }) ),
+    // takeWhile( ({ y }) => y <= 150 )
+    // Display the value that satisfies the condition
+    takeWhile( ({ y }) => y <= 150, true )
+)
+.subscribe({
+    next: val => console.log('next:', val),
+    complete: () => console.log('complete'),
+});
